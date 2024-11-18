093ed82b9b8ca83c0fe8fda5e2d66381cbe7a8e5
kerry <dingyong87@163.com>
2024-11-18 17:40:47 +0800

    feat: add bitmap some commands and unit tests.

--- src/main/java/io/velo/command/BGroup.java
+++ src/main/java/io/velo/command/BGroup.java
@@ -2,11 +2,11 @@ package io.velo.command;
 
 import io.activej.net.socket.tcp.ITcpSocket;
 import io.velo.BaseCommand;
-import io.velo.reply.NilReply;
-import io.velo.reply.OKReply;
-import io.velo.reply.Reply;
+import io.velo.reply.*;
+import org.jetbrains.annotations.VisibleForTesting;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 
 public class BGroup extends BaseCommand {
     public BGroup(String cmd, byte[][] data, ITcpSocket socket) {
@@ -15,15 +15,190 @@ public class BGroup extends BaseCommand {
 
     public ArrayList<SlotWithKeyHash> parseSlots(String cmd, byte[][] data, int slotNumber) {
         ArrayList<SlotWithKeyHash> slotWithKeyHashList = new ArrayList<>();
+        if ("bitcount".equals(cmd) || "bitfield".equals(cmd) || "bitfield_ro".equals(cmd) ||
+                "bitpos".equals(cmd)) {
+            if (data.length < 2) {
+                return slotWithKeyHashList;
+            }
+            var keyBytes = data[1];
+            var slotWithKeyHash = slot(keyBytes, slotNumber);
+            slotWithKeyHashList.add(slotWithKeyHash);
+            return slotWithKeyHashList;
+        }
+
+        if ("bitop".equals(cmd)) {
+            if (data.length < 5) {
+                return slotWithKeyHashList;
+            }
+
+            // begin with dst key
+            // eg. bitop and dest src1 src2
+            for (int i = 2; i < data.length; i++) {
+                var keyBytes = data[i];
+                var slotWithKeyHash = slot(keyBytes, slotNumber);
+                slotWithKeyHashList.add(slotWithKeyHash);
+            }
+            return slotWithKeyHashList;
+        }
+
         return slotWithKeyHashList;
     }
 
     public Reply handle() {
+        if ("bitcount".equals(cmd)) {
+            return bitcount();
+        }
+
+        if ("bitpos".equals(cmd)) {
+            return bitpos();
+        }
+
         if ("bgsave".equals(cmd)) {
-            // already saved when handle request
+            // pure memory need to flush to disk, todo
             return OKReply.INSTANCE;
         }
 
         return NilReply.INSTANCE;
+    }
+
+    @VisibleForTesting
+    Reply bitcount() {
+        if (data.length < 2) {
+            return ErrorReply.FORMAT;
+        }
+
+        var keyBytes = data[1];
+
+        int start;
+        int end;
+        boolean isIndexUseBit;
+        if (data.length == 2) {
+            start = 0;
+            end = -1;
+            isIndexUseBit = false;
+        } else {
+            if (data.length != 4 && data.length != 5) {
+                return ErrorReply.SYNTAX;
+            }
+
+            try {
+                start = Integer.parseInt(new String(data[2]));
+                end = Integer.parseInt(new String(data[3]));
+            } catch (NumberFormatException e) {
+                return ErrorReply.NOT_INTEGER;
+            }
+
+            isIndexUseBit = data.length == 5 && "bit".equalsIgnoreCase(new String(data[4]));
+        }
+
+        var slotWithKeyHash = slotWithKeyHashListParsed.getFirst();
+        var cv = getCv(keyBytes, slotWithKeyHash);
+        if (cv == null) {
+            return IntegerReply.REPLY_0;
+        }
+
+        if (!cv.isTypeString()) {
+            return ErrorReply.WRONG_TYPE;
+        }
+
+        var valueBytes = getValueBytesByCv(cv, keyBytes, slotWithKeyHash);
+        var canIndexValueBytesLength = isIndexUseBit ? valueBytes.length * 8 : valueBytes.length;
+
+        var startEndWith = IndexStartEndReset.reset(start, end, canIndexValueBytesLength);
+        if (!startEndWith.valid()) {
+            return IntegerReply.REPLY_0;
+        }
+
+        var bitset = BitSet.valueOf(valueBytes);
+        int count = 0;
+        if (isIndexUseBit) {
+            for (int i = startEndWith.start(); i <= startEndWith.end(); i++) {
+                if (bitset.get(i)) {
+                    count++;
+                }
+            }
+        } else {
+            for (int i = startEndWith.start() * 8; i <= (startEndWith.end() + 1) * 8 - 1; i++) {
+                if (bitset.get(i)) {
+                    count++;
+                }
+            }
+        }
+
+        return new IntegerReply(count);
+    }
+
+    @VisibleForTesting
+    Reply bitpos() {
+        if (data.length < 3) {
+            return ErrorReply.FORMAT;
+        }
+
+        var keyBytes = data[1];
+        var bit1or0Bytes = data[2];
+        var isBit1 = "1".equals(new String(bit1or0Bytes));
+        if (!isBit1 && !"0".equals(new String(bit1or0Bytes))) {
+            return ErrorReply.SYNTAX;
+        }
+
+        int start;
+        int end;
+        boolean isIndexUseBit;
+        if (data.length == 3) {
+            start = 0;
+            end = -1;
+            isIndexUseBit = false;
+        } else {
+            if (data.length != 4 && data.length != 5 && data.length != 6) {
+                return ErrorReply.SYNTAX;
+            }
+
+            try {
+                start = Integer.parseInt(new String(data[3]));
+                end = data.length >= 5 ? Integer.parseInt(new String(data[4])) : -1;
+            } catch (NumberFormatException e) {
+                return ErrorReply.NOT_INTEGER;
+            }
+
+            isIndexUseBit = data.length == 6 && "bit".equalsIgnoreCase(new String(data[5]));
+        }
+
+        var slotWithKeyHash = slotWithKeyHashListParsed.getFirst();
+        var cv = getCv(keyBytes, slotWithKeyHash);
+        if (cv == null) {
+            return new IntegerReply(-1);
+        }
+
+        if (!cv.isTypeString()) {
+            return ErrorReply.WRONG_TYPE;
+        }
+
+        var valueBytes = getValueBytesByCv(cv, keyBytes, slotWithKeyHash);
+        var canIndexValueBytesLength = isIndexUseBit ? valueBytes.length * 8 : valueBytes.length;
+
+        var startEndWith = IndexStartEndReset.reset(start, end, canIndexValueBytesLength);
+        if (!startEndWith.valid()) {
+            return new IntegerReply(-1);
+        }
+
+        var bitset = BitSet.valueOf(valueBytes);
+        int pos = -1;
+        if (isIndexUseBit) {
+            for (int i = startEndWith.start(); i <= startEndWith.end(); i++) {
+                if ((bitset.get(i) && isBit1) || (!bitset.get(i) && !isBit1)) {
+                    pos = i;
+                    break;
+                }
+            }
+        } else {
+            for (int i = startEndWith.start() * 8; i <= (startEndWith.end() + 1) * 8 - 1; i++) {
+                if ((bitset.get(i) && isBit1) || (!bitset.get(i) && !isBit1)) {
+                    pos = i;
+                    break;
+                }
+            }
+        }
+
+        return new IntegerReply(pos);
     }
 }
--- src/test/groovy/io/velo/command/BGroupTest.groovy
+++ src/test/groovy/io/velo/command/BGroupTest.groovy
@@ -1,37 +1,263 @@
 package io.velo.command
 
 import io.velo.BaseCommand
+import io.velo.CompressedValue
+import io.velo.mock.InMemoryGetSet
+import io.velo.persist.Mock
+import io.velo.reply.ErrorReply
+import io.velo.reply.IntegerReply
 import io.velo.reply.NilReply
 import io.velo.reply.OKReply
 import spock.lang.Specification
 
 class BGroupTest extends Specification {
     def _BGroup = new BGroup(null, null, null)
+    final short slot = 0
 
     def 'test parse slot'() {
         given:
-        def data1 = new byte[1][]
+        def data2 = new byte[2][]
         int slotNumber = 128
 
+        and:
+        data2[1] = 'a'.bytes
+
         when:
-        def sBgsaveList = _BGroup.parseSlots('bgsave', data1, slotNumber)
+        def sList = _BGroup.parseSlots('bitcount', data2, slotNumber)
         then:
-        sBgsaveList.size() == 0
+        sList.size() == 1
+
+        when:
+        def data1 = new byte[1][]
+        sList = _BGroup.parseSlots('bitcount', data1, slotNumber)
+        then:
+        sList.size() == 0
+
+        when:
+        sList = _BGroup.parseSlots('bitfield', data2, slotNumber)
+        then:
+        sList.size() == 1
+
+        when:
+        sList = _BGroup.parseSlots('bitfield_ro', data2, slotNumber)
+        then:
+        sList.size() == 1
+
+        when:
+        sList = _BGroup.parseSlots('bitpos', data2, slotNumber)
+        then:
+        sList.size() == 1
+
+        when:
+        sList = _BGroup.parseSlots('bitop', data2, slotNumber)
+        then:
+        sList.size() == 0
+
+        when:
+        def data5 = new byte[5][]
+        data5[1] = 'and'.bytes
+        data5[2] = 'a'.bytes
+        data5[3] = 'b'.bytes
+        data5[4] = 'c'.bytes
+        sList = _BGroup.parseSlots('bitop', data5, slotNumber)
+        then:
+        sList.size() == 3
+
+        when:
+        sList = _BGroup.parseSlots('bgsave', data2, slotNumber)
+        then:
+        sList.size() == 0
     }
 
     def 'test handle'() {
         given:
-        def bGroup = new BGroup(null, null, null)
+        def data1 = new byte[1][]
+
+        def bGroup = new BGroup('bitcount', data1, null)
         bGroup.from(BaseCommand.mockAGroup())
 
         when:
-        def reply = bGroup.execute('bgsave')
+        def reply = bGroup.handle()
+        then:
+        reply == ErrorReply.FORMAT
+
+        when:
+        bGroup.cmd = 'bitpos'
+        reply = bGroup.handle()
+        then:
+        reply == ErrorReply.FORMAT
+
+        when:
+        bGroup.cmd = 'bgsave'
+        reply = bGroup.handle()
         then:
         reply == OKReply.INSTANCE
 
         when:
-        reply = bGroup.execute('zzz')
+        bGroup.cmd = 'zzz'
+        reply = bGroup.handle()
         then:
         reply == NilReply.INSTANCE
+    }
+
+    def 'test bitcount'() {
+        given:
+        def inMemoryGetSet = new InMemoryGetSet()
+
+        def bGroup = new BGroup(null, null, null)
+        bGroup.byPassGetSet = inMemoryGetSet
+        bGroup.from(BaseCommand.mockAGroup())
+
+        when:
+        inMemoryGetSet.remove(slot, 'a')
+        def reply = bGroup.execute('bitcount a')
+        then:
+        reply == IntegerReply.REPLY_0
+
+        when:
+        def cv = Mock.prepareCompressedValueList(1)[0]
+        cv.compressedData = 'foobar'.bytes
+        cv.compressedLength = 6
+        inMemoryGetSet.put(slot, 'a', 0, cv)
+        reply = bGroup.execute('bitcount a')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == 26
+
+        when:
+        reply = bGroup.execute('bitcount a 0 0')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == 4
+
+        when:
+        reply = bGroup.execute('bitcount a 0 -1')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == 26
+
+        when:
+        reply = bGroup.execute('bitcount a 2 1')
+        then:
+        reply == IntegerReply.REPLY_0
+
+        when:
+        reply = bGroup.execute('bitcount a 1 1 byte')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == 6
+
+        when:
+        reply = bGroup.execute('bitcount a 5 30 bit')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == 17
+
+        when:
+        cv.dictSeqOrSpType = CompressedValue.SP_TYPE_HASH
+        inMemoryGetSet.put(slot, 'a', 0, cv)
+        reply = bGroup.execute('bitcount a')
+        then:
+        reply == ErrorReply.WRONG_TYPE
+
+        when:
+        reply = bGroup.execute('bitcount a b 5')
+        then:
+        reply == ErrorReply.NOT_INTEGER
+
+        when:
+        reply = bGroup.execute('bitcount a 5 30 bit x')
+        then:
+        reply == ErrorReply.SYNTAX
+    }
+
+    def 'test bitpos'() {
+        given:
+        def inMemoryGetSet = new InMemoryGetSet()
+
+        def bGroup = new BGroup(null, null, null)
+        bGroup.byPassGetSet = inMemoryGetSet
+        bGroup.from(BaseCommand.mockAGroup())
+
+        when:
+        inMemoryGetSet.remove(slot, 'a')
+        def reply = bGroup.execute('bitpos a 1')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == -1
+
+        when:
+        def cv = Mock.prepareCompressedValueList(1)[0]
+        cv.compressedData = 'foobar'.bytes
+        cv.compressedLength = 6
+        inMemoryGetSet.put(slot, 'a', 0, cv)
+        reply = bGroup.execute('bitpos a 0')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 0 0 -1')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 0 0')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 0 2 1')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer == -1
+
+        when:
+        reply = bGroup.execute('bitpos a 0 0 -1 byte')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 1 0 -1 byte')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 0 0 -1 bit')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        reply = bGroup.execute('bitpos a 1 0 -1 bit')
+        then:
+        reply instanceof IntegerReply
+        (reply as IntegerReply).integer != -1
+
+        when:
+        cv.dictSeqOrSpType = CompressedValue.SP_TYPE_HASH
+        inMemoryGetSet.put(slot, 'a', 0, cv)
+        reply = bGroup.execute('bitpos a 1')
+        then:
+        reply == ErrorReply.WRONG_TYPE
+
+        when:
+        reply = bGroup.execute('bitpos a 1 _ _')
+        then:
+        reply == ErrorReply.NOT_INTEGER
+
+        when:
+        reply = bGroup.execute('bitpos a 1 _ _ _ _')
+        then:
+        reply == ErrorReply.SYNTAX
+
+        when:
+        reply = bGroup.execute('bitpos a 2 0 -1')
+        then:
+        reply == ErrorReply.SYNTAX
     }
 }
