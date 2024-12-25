cf3159c2646f6ec3bea61fc143b47f23e7e2f440
kerry <dingyong87@163.com>
2024-12-25 12:24:40 +0800

    build: add miss doc annotation.

---
+++ src/main/java/io/velo/NullableOnlyTest.java
@@ -0,0 +1,12 @@
+package io.velo;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.PARAMETER})
+public @interface NullableOnlyTest {
+}
+
