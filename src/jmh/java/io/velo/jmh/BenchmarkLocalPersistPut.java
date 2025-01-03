a70751574a4c76bf37dc08d3c4a326c927a48506
kerry <dingyong87@163.com>
2025-01-03 20:47:33 +0800

    test: add local persist multi-threads put benchmark test.

---
+++ src/jmh/java/io/velo/jmh/BenchmarkLocalPersistPut.java
@@ -0,0 +1,203 @@
+package io.velo.jmh;
+
+import io.activej.config.Config;
+import io.activej.eventloop.Eventloop;
+import io.velo.*;
+import io.velo.persist.LocalPersist;
+import org.apache.commons.io.FileUtils;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@BenchmarkMode({Mode.AverageTime})
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@Warmup(iterations = 0, time = 10)
+@Measurement(iterations = 1, time = 10)
+@State(Scope.Benchmark)
+@Threads(1)
+public class BenchmarkLocalPersistPut {
+    private String[] keys;
+
+    int keyNumber = 10_000_000;
+
+    private final File persistDir = new File("/tmp/test_jmh_persist");
+
+    private final LocalPersist localPersist = LocalPersist.getInstance();
+
+    short slotNumber = 4;
+
+    byte netWorkers = 4;
+
+    private Eventloop[] netWorkerEventloopArray;
+
+    private AtomicInteger count = new AtomicInteger();
+    private long beginTimeMs;
+    private long beginTimeMs2;
+
+    @Setup
+    public void setup() throws IOException, InterruptedException {
+        keys = new String[keyNumber];
+        for (int i = 0; i < keyNumber; i++) {
+            keys[i] = "key:" + Utils.leftPad(String.valueOf(i), "0", 12);
+        }
+        System.out.printf("init keys, number: %d\n", keyNumber);
+
+        // todo, change here
+        ConfForSlot.global = ConfForSlot.c10m;
+
+        netWorkerEventloopArray = new Eventloop[netWorkers];
+        for (int i = 0; i < netWorkers; i++) {
+            var eventloop = Eventloop.builder()
+                    .withThreadName("net-worker-" + i)
+                    .withIdleInterval(Duration.ofMillis(10))
+                    .build();
+            eventloop.keepAlive(true);
+            netWorkerEventloopArray[i] = eventloop;
+
+            new Thread(eventloop).start();
+            Thread.sleep(100);
+            System.out.println("net worker event loop started, thread id: " + eventloop.getEventloopThread().threadId());
+        }
+
+        ConfForGlobal.netListenAddresses = "127.0.0.1:7379";
+        RequestHandler.initMultiShardShadows(netWorkers);
+
+        if (persistDir.exists()) {
+            FileUtils.deleteDirectory(persistDir);
+            FileUtils.forceMkdir(persistDir);
+            System.out.println("delete and recreate persist dir");
+        }
+
+        var snowFlakes = new SnowFlake[netWorkers];
+        for (int i = 0; i < netWorkers; i++) {
+            snowFlakes[i] = new SnowFlake(ConfForGlobal.datacenterId, (ConfForGlobal.machineId << 8) | i);
+        }
+        localPersist.initSlots(netWorkers, slotNumber, snowFlakes, persistDir, Config.create());
+        System.out.println("init local persist slots");
+
+        for (int i = 0; i < netWorkers; i++) {
+            for (var oneSlot : localPersist.oneSlots()) {
+                if (oneSlot.slot() % netWorkers == i) {
+                    oneSlot.setNetWorkerEventloop(netWorkerEventloopArray[i]);
+                    System.out.printf("set net worker event loop for slot=%d, net worker id=%d\n", oneSlot.slot(), i);
+                }
+            }
+        }
+
+        for (short slot = 0; slot < slotNumber; slot++) {
+            int i = slot % netWorkers;
+            localPersist.fixSlotThreadId(slot, netWorkerEventloopArray[i].getEventloopThread().threadId());
+        }
+
+        beginTimeMs = System.currentTimeMillis();
+        beginTimeMs2 = System.currentTimeMillis();
+    }
+
+    @TearDown
+    public void tearDown() throws InterruptedException {
+        // 30s
+        final int maxLoopCount = 10 * 30;
+        int loopCount = 0;
+        while (count.get() < keyNumber) {
+            Thread.sleep(100);
+            loopCount++;
+
+            if (loopCount > maxLoopCount) {
+                throw new RuntimeException("timeout");
+            }
+        }
+
+        var costTimeMs = System.currentTimeMillis() - beginTimeMs;
+        System.out.printf("put %d keys, cost time: %d ms\n", keyNumber, costTimeMs);
+
+        var qps = keyNumber * 1000L / costTimeMs;
+        System.out.printf("qps: %d\n", qps);
+
+        isAfterTearDown = true;
+
+        for (var eventloop : netWorkerEventloopArray) {
+            eventloop.breakEventloop();
+        }
+        System.out.println("break net worker event loops");
+
+        System.out.println("wait 10s when break net worker event loops and then clean up local persist");
+        Thread.sleep(1000 * 10);
+
+        localPersist.cleanUp();
+        System.out.println("clean up local persist");
+    }
+
+    private volatile boolean isAfterTearDown = false;
+
+    private final Random random = new Random();
+
+    // -d 200
+//    private final byte[] valueBytes = Utils.leftPad("value", "0", 200).getBytes();
+    // short values, value length <= 32
+    // -d 5
+    private final byte[] valueBytes = "value".getBytes();
+
+    /*
+    -d 5
+    qps: 989902
+
+Benchmark                     Mode  Cnt  Score   Error  Units
+BenchmarkLocalPersistPut.put  avgt       0.590          us/op
+
+    -d 200
+    qps: 435293
+
+Benchmark                     Mode  Cnt  Score   Error  Units
+BenchmarkLocalPersistPut.put  avgt       0.629          us/op
+
+TIPS: 512MB write buffer for each slot, so the real write qps is in logs.
+     */
+
+    @Benchmark
+    public void put() {
+        var key = keys[random.nextInt(keyNumber)];
+        var s = BaseCommand.slot(key.getBytes(), slotNumber);
+        var oneSlot = localPersist.oneSlot(s.slot());
+        var eventloop = netWorkerEventloopArray[s.slot() % netWorkers];
+
+        eventloop.submit(() -> {
+            if (isAfterTearDown) {
+                return;
+            }
+
+            var cv = new CompressedValue();
+            cv.setSeq(1L);
+            cv.setKeyHash(s.keyHash());
+            cv.setExpireAt(CompressedValue.NO_EXPIRE);
+            cv.setDictSeqOrSpType(CompressedValue.NULL_DICT_SEQ);
+            cv.setCompressedLength(valueBytes.length);
+            cv.setUncompressedLength(valueBytes.length);
+            cv.setCompressedData(valueBytes);
+
+            oneSlot.put(key, s.bucketIndex(), cv);
+
+            var c = count.incrementAndGet();
+            if (c % 1_000_000 == 0) {
+                var costTimeMs2 = System.currentTimeMillis() - beginTimeMs2;
+                System.out.printf("put %d keys, 100w cost time: %d ms, qps: %d\n", c, costTimeMs2, 1_000_000_000L / costTimeMs2);
+                beginTimeMs2 = System.currentTimeMillis();
+            }
+        });
+    }
+
+    public static void main(String[] args) throws RunnerException {
+        var opt = new OptionsBuilder()
+                .include(BenchmarkLocalPersistPut.class.getSimpleName())
+                .forks(1)
+                .build();
+        new Runner(opt).run();
+    }
+}
