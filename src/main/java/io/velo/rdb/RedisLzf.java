a6651b6795bbd55a447b84409505907c96f42bf8
kerry <dingyong87@163.com>
2025-01-21 14:17:12 +0800

    feat: add redis lzf compress/decompress use JNA.

---
+++ src/main/java/io/velo/rdb/RedisLzf.java
@@ -0,0 +1,14 @@
+package io.velo.rdb;
+
+import com.sun.jna.Library;
+import com.sun.jna.Native;
+
+import java.nio.Buffer;
+
+public interface RedisLzf extends Library {
+    RedisLzf instance = Native.loadLibrary("./libredis_lzf.so", RedisLzf.class);
+
+    int lzf_compress(final Buffer input, int inputLength, Buffer output, int outputLength);
+
+    int lzf_decompress(final Buffer input, int inputLength, Buffer output, int outputLength);
+}
