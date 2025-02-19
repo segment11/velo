3ed2aa592645c26702d906e379cf1686cedb09c0
kerry <dingyong87@163.com>
2025-02-19 14:04:18 +0800

    doc: add java docs.

--- src/main/java/io/velo/Dict.java
+++ src/main/java/io/velo/Dict.java
@@ -17,19 +17,51 @@ import java.util.Objects;
 import java.util.Random;
 import java.util.concurrent.atomic.LongAdder;
 
+/**
+ * Represents a dictionary used for compression and decompression with Zstd.
+ * This class manages dictionary bytes, compression contexts, and provides methods for encoding and decoding dictionaries.
+ */
 public class Dict implements Serializable {
+
+    /**
+     * Sequence number for the self Zstd dictionary.
+     */
     public static final int SELF_ZSTD_DICT_SEQ = 1;
+
+    /**
+     * Sequence number for the global Zstd dictionary.
+     */
     public static final int GLOBAL_ZSTD_DICT_SEQ = 10;
 
+    /**
+     * Singleton instance of the self Zstd dictionary.
+     */
     public static final Dict SELF_ZSTD_DICT = new Dict();
+
+    /**
+     * Singleton instance of the global Zstd dictionary.
+     */
     public static final Dict GLOBAL_ZSTD_DICT = new Dict();
-    // warning: this key can not be other dict key prefix
+
+    /**
+     * Key for the global Zstd dictionary.
+     * Note: This key should not be a prefix of any other dictionary key.
+     */
     public static final String GLOBAL_ZSTD_DICT_KEY = "dict-x-global";
 
+    /**
+     * File name for the global dictionary file.
+     */
     static final String GLOBAL_DICT_FILE_NAME = "dict-global-raw.dat";
-    // for latency
+
+    /**
+     * Maximum length of the global dictionary bytes for latency considerations.
+     */
     private static final int GLOBAL_DICT_BYTES_MAX_LENGTH = 1024 * 16;
 
+    /**
+     * Logger for logging information and warnings.
+     */
     private static final Logger log = LoggerFactory.getLogger(Dict.class);
 
     static {
@@ -37,6 +69,12 @@ public class Dict implements Serializable {
         GLOBAL_ZSTD_DICT.seq = GLOBAL_ZSTD_DICT_SEQ;
     }
 
+    /**
+     * Resets the global dictionary bytes.
+     *
+     * @param dictBytes the new dictionary bytes
+     * @throws IllegalStateException if the dictionary bytes are too long or empty
+     */
     public static void resetGlobalDictBytes(byte[] dictBytes) {
         if (dictBytes.length == 0 || dictBytes.length > GLOBAL_DICT_BYTES_MAX_LENGTH) {
             throw new IllegalStateException("Dict global dict bytes too long=" + dictBytes.length);
@@ -48,6 +86,11 @@ public class Dict implements Serializable {
         GLOBAL_ZSTD_DICT.initCtx();
     }
 
+    /**
+     * Initializes the global dictionary bytes from a file.
+     *
+     * @param targetFile the file containing the dictionary bytes
+     */
     public static void initGlobalDictBytesByFile(File targetFile) {
         if (!targetFile.exists()) {
             log.warn("Dict global dict file not exists={}", targetFile.getAbsolutePath());
@@ -63,6 +106,11 @@ public class Dict implements Serializable {
         }
     }
 
+    /**
+     * Saves the global dictionary bytes to a file.
+     *
+     * @param targetFile the file to save the dictionary bytes to
+     */
     public static void saveGlobalDictBytesToFile(File targetFile) {
         try {
             Files.write(targetFile.toPath(), GLOBAL_ZSTD_DICT.dictBytes);
@@ -71,45 +119,104 @@ public class Dict implements Serializable {
         }
     }
 
+    /**
+     * Sequence number of the dictionary.
+     */
     private int seq;
 
+    /**
+     * Returns the sequence number of the dictionary.
+     *
+     * @return the sequence number
+     */
     public int getSeq() {
         return seq;
     }
 
+    /**
+     * Sets the sequence number of the dictionary.
+     *
+     * @param seq the sequence number to set
+     */
     public void setSeq(int seq) {
         this.seq = seq;
     }
 
+    /**
+     * Creation time of the dictionary.
+     */
     private long createdTime;
 
+    /**
+     * Returns the creation time of the dictionary.
+     *
+     * @return the creation time
+     */
     public long getCreatedTime() {
         return createdTime;
     }
 
+    /**
+     * Sets the creation time of the dictionary.
+     *
+     * @param createdTime the creation time to set
+     */
     public void setCreatedTime(long createdTime) {
         this.createdTime = createdTime;
     }
 
+    /**
+     * Dictionary bytes used for compression and decompression.
+     */
     private byte[] dictBytes;
 
+    /**
+     * Returns the dictionary bytes.
+     *
+     * @return the dictionary bytes
+     */
     public byte[] getDictBytes() {
         return dictBytes;
     }
 
+    /**
+     * Checks if the dictionary has valid dictionary bytes.
+     *
+     * @return true if the dictionary has valid dictionary bytes, false otherwise
+     */
     public boolean hasDictBytes() {
-        // global / self dict, dict bytes length is 1
         return dictBytes != null && dictBytes.length > 1;
     }
 
+    /**
+     * Sets the dictionary bytes.
+     *
+     * @param dictBytes the dictionary bytes to set
+     */
     public void setDictBytes(byte[] dictBytes) {
         this.dictBytes = dictBytes;
     }
 
+    /**
+     * Total count of compressed data.
+     */
     final LongAdder compressedCountTotal = new LongAdder();
+
+    /**
+     * Total bytes of compressed data.
+     */
     final LongAdder compressedBytesTotal = new LongAdder();
+
+    /**
+     * Total bytes of data before compression.
+     */
     final LongAdder compressBytesTotal = new LongAdder();
 
+    /**
+     * Calculates the compression ratio.
+     *
+     * @return the compression ratio
+     */
     double compressedRatio() {
         var sumCompress = compressBytesTotal.sum();
         if (sumCompress == 0) {
@@ -120,13 +227,23 @@ public class Dict implements Serializable {
         return (double) sumCompressed / sumCompress;
     }
 
+    /**
+     * Decompression contexts for each thread.
+     */
     @VisibleForTesting
     @ThreadNeedLocal
     ZstdDecompressCtx[] decompressCtxArray;
+
+    /**
+     * Compression contexts for each thread.
+     */
     @VisibleForTesting
     @ThreadNeedLocal
     ZstdCompressCtx[] ctxCompressArray;
 
+    /**
+     * Initializes the compression and decompression contexts.
+     */
     void initCtx() {
         if (decompressCtxArray == null) {
             decompressCtxArray = new ZstdDecompressCtx[ConfForGlobal.netWorkers];
@@ -147,6 +264,9 @@ public class Dict implements Serializable {
         }
     }
 
+    /**
+     * Closes the compression and decompression contexts.
+     */
     void closeCtx() {
         if (decompressCtxArray != null) {
             for (var zstdDecompressCtx : decompressCtxArray) {
@@ -165,6 +285,12 @@ public class Dict implements Serializable {
         }
     }
 
+    /**
+     * Compresses a byte array.
+     *
+     * @param src the source byte array to compress
+     * @return the compressed byte array
+     */
     public byte[] compressByteArray(byte[] src) {
         var r = ctxCompressArray[MultiWorkerServer.STATIC_GLOBAL_V.getThreadLocalIndexByCurrentThread()].compress(src);
         compressedCountTotal.add(1);
@@ -173,6 +299,16 @@ public class Dict implements Serializable {
         return r;
     }
 
+    /**
+     * Compresses a byte array into a destination byte array.
+     *
+     * @param dst       the destination byte array
+     * @param dstOffset the offset in the destination byte array
+     * @param src       the source byte array to compress
+     * @param srcOffset the offset in the source byte array
+     * @param length    the number of bytes to compress
+     * @return the number of bytes written to the destination array
+     */
     public int compressByteArray(byte[] dst, int dstOffset, byte[] src, int srcOffset, int length) {
         var n = ctxCompressArray[MultiWorkerServer.STATIC_GLOBAL_V.getThreadLocalIndexByCurrentThread()].compressByteArray(dst, dstOffset, dst.length - dstOffset, src, srcOffset, length);
         compressedCountTotal.add(1);
@@ -181,10 +317,31 @@ public class Dict implements Serializable {
         return n;
     }
 
+    /**
+     * Decompresses a byte array into a destination byte array.
+     *
+     * @param dst       the destination byte array
+     * @param dstOffset the offset in the destination byte array
+     * @param src       the source byte array to decompress
+     * @param srcOffset the offset in the source byte array
+     * @param length    the number of bytes to decompress
+     * @return the number of bytes written to the destination array
+     */
     public int decompressByteArray(byte[] dst, int dstOffset, byte[] src, int srcOffset, int length) {
         return decompressCtxArray[MultiWorkerServer.STATIC_GLOBAL_V.getThreadLocalIndexByCurrentThread()].decompressByteArray(dst, dstOffset, dst.length - dstOffset, src, srcOffset, length);
     }
 
+    /**
+     * Compresses a direct byte buffer.
+     *
+     * @param dstBuffer the destination byte buffer
+     * @param dstOffset the offset in the destination byte buffer
+     * @param dstSize   the size of the destination byte buffer
+     * @param srcBuffer the source byte buffer to compress
+     * @param srcOffset the offset in the source byte buffer
+     * @param length    the number of bytes to compress
+     * @return the number of bytes written to the destination buffer
+     */
     public int compressByteBuffer(ByteBuffer dstBuffer, int dstOffset, int dstSize, ByteBuffer srcBuffer, int srcOffset, int length) {
         var n = ctxCompressArray[MultiWorkerServer.STATIC_GLOBAL_V.getThreadLocalIndexByCurrentThread()].compressDirectByteBuffer(dstBuffer, dstOffset, dstSize, srcBuffer, srcOffset, length);
         compressedCountTotal.add(1);
@@ -193,10 +350,26 @@ public class Dict implements Serializable {
         return n;
     }
 
+    /**
+     * Decompresses a direct byte buffer.
+     *
+     * @param dstBuffer the destination byte buffer
+     * @param dstOffset the offset in the destination byte buffer
+     * @param dstSize   the size of the destination byte buffer
+     * @param srcBuffer the source byte buffer to decompress
+     * @param srcOffset the offset in the source byte buffer
+     * @param length    the number of bytes to decompress
+     * @return the number of bytes written to the destination buffer
+     */
     public int decompressByteBuffer(ByteBuffer dstBuffer, int dstOffset, int dstSize, ByteBuffer srcBuffer, int srcOffset, int length) {
         return decompressCtxArray[MultiWorkerServer.STATIC_GLOBAL_V.getThreadLocalIndexByCurrentThread()].decompressDirectByteBuffer(dstBuffer, dstOffset, dstSize, srcBuffer, srcOffset, length);
     }
 
+    /**
+     * Returns a string representation of the dictionary.
+     *
+     * @return a string representation of the dictionary
+     */
     @Override
     public String toString() {
         return "Dict{" +
@@ -206,11 +379,22 @@ public class Dict implements Serializable {
                 '}';
     }
 
+    /**
+     * Returns the hash code of the dictionary.
+     *
+     * @return the hash code
+     */
     @Override
     public int hashCode() {
         return Objects.hashCode(seq);
     }
 
+    /**
+     * Checks if this dictionary is equal to another object.
+     *
+     * @param obj the object to compare to
+     * @return true if the objects are equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
@@ -219,15 +403,29 @@ public class Dict implements Serializable {
         return seq == dict.seq;
     }
 
-    // seq int + create time long + key prefix length short + key prefix + dict bytes length short + dict bytes
+    /**
+     * Length of the encoded dictionary header.
+     */
     private static final int ENCODED_HEADER_LENGTH = 4 + 8 + 2 + 2;
 
+    /**
+     * Calculates the length of the encoded dictionary.
+     *
+     * @param keyPrefix the key prefix to include in the encoding
+     * @return the length of the encoded dictionary
+     */
     public int encodeLength(String keyPrefix) {
         return 4 + ENCODED_HEADER_LENGTH + keyPrefix.length() + dictBytes.length;
     }
 
-    public byte[] encode(String keyPrefix) {
-        int vLength = ENCODED_HEADER_LENGTH + keyPrefix.length() + dictBytes.length;
+    /**
+     * Encodes the dictionary with a key prefix or suffix.
+     *
+     * @param keyPrefixOrSuffix the key prefix to include in the encoding
+     * @return the encoded dictionary bytes
+     */
+    public byte[] encode(String keyPrefixOrSuffix) {
+        int vLength = ENCODED_HEADER_LENGTH + keyPrefixOrSuffix.length() + dictBytes.length;
 
         var bytes = new byte[4 + vLength];
         var buffer = ByteBuffer.wrap(bytes);
@@ -235,15 +433,23 @@ public class Dict implements Serializable {
         buffer.putInt(vLength);
         buffer.putInt(seq);
         buffer.putLong(createdTime);
-        buffer.putShort((short) keyPrefix.length());
-        buffer.put(keyPrefix.getBytes());
+        buffer.putShort((short) keyPrefixOrSuffix.length());
+        buffer.put(keyPrefixOrSuffix.getBytes());
         buffer.putShort((short) dictBytes.length);
         buffer.put(dictBytes);
 
         return bytes;
     }
 
+    /**
+     * Record representing a dictionary with a key prefix or suffix.
+     */
     public record DictWithKeyPrefixOrSuffix(String keyPrefixOrSuffix, Dict dict) {
+        /**
+         * Returns a string representation of the dictionary with key prefix or suffix.
+         *
+         * @return a string representation of the dictionary with key prefix or suffix
+         */
         @Override
         public String toString() {
             return "DictWithKeyPrefixOrSuffix{" +
@@ -253,6 +459,13 @@ public class Dict implements Serializable {
         }
     }
 
+    /**
+     * Decodes a dictionary from a data input stream.
+     *
+     * @param is the data input stream to read from
+     * @return the decoded dictionary with key prefix or suffix
+     * @throws IOException if an I/O error occurs
+     */
     public static DictWithKeyPrefixOrSuffix decode(DataInputStream is) throws IOException {
         if (is.available() < 4) {
             return null;
@@ -288,13 +501,21 @@ public class Dict implements Serializable {
         return new DictWithKeyPrefixOrSuffix(new String(keyPrefixOrSuffixBytes), dict);
     }
 
+    /**
+     * Constructs a new dictionary with default values.
+     */
     public Dict() {
+        // one dict byte means not set yet
         this.dictBytes = new byte[1];
         this.seq = SELF_ZSTD_DICT_SEQ;
         this.createdTime = System.currentTimeMillis();
     }
 
-    // still may be conflict, when slave change to master, new master create new dict with same seq
+    /**
+     * Generates a random sequence number for a new dictionary.
+     *
+     * @return a random sequence number
+     */
     static int generateRandomSeq() {
         var random = new Random();
         return random.nextInt(1000) * 1000 * 1000 +
@@ -303,7 +524,12 @@ public class Dict implements Serializable {
                 GLOBAL_ZSTD_DICT_SEQ;
     }
 
-    // only create when train new dict by TrainSampleJob
+    /**
+     * Constructs a new dictionary with the specified dictionary bytes.
+     *
+     * @param dictBytes the dictionary bytes
+     * @throws IllegalArgumentException if the dictionary bytes are too long
+     */
     public Dict(byte[] dictBytes) {
         if (dictBytes.length > Short.MAX_VALUE) {
             throw new IllegalArgumentException("Dict bytes too long=" + dictBytes.length);
--- src/main/java/io/velo/DictMap.java
+++ src/main/java/io/velo/DictMap.java
@@ -17,10 +17,25 @@ import java.util.List;
 import java.util.TreeMap;
 import java.util.concurrent.ConcurrentHashMap;
 
+/**
+ * Manages a map of dictionaries used for compression and decompression.
+ * This class provides methods for adding, retrieving, and managing dictionaries, as well as handling metrics and persistence.
+ */
 public class DictMap implements NeedCleanUp {
+
+    /**
+     * Minimum data length to trigger compression.
+     */
     public static int TO_COMPRESS_MIN_DATA_LENGTH = 64;
+
+    /**
+     * Minimum data length to use the self dictionary for compression.
+     */
     public static int TO_COMPRESS_USE_SELF_DICT_MIN_DATA_LENGTH = 256;
 
+    /**
+     * Gauge for tracking dictionary compression metrics.
+     */
     @VisibleForTesting
     final static SimpleGauge dictCompressedGauge = new SimpleGauge("dict", "Dict compressed metrics.", "name");
 
@@ -28,33 +43,75 @@ public class DictMap implements NeedCleanUp {
         dictCompressedGauge.register();
     }
 
-    // singleton
+    /**
+     * Singleton instance of DictMap.
+     */
     private static final DictMap instance = new DictMap();
 
+    /**
+     * Returns the singleton instance of DictMap.
+     *
+     * @return the singleton instance of DictMap
+     */
     public static DictMap getInstance() {
         return instance;
     }
 
+    /**
+     * Private constructor to enforce singleton pattern.
+     */
     private DictMap() {
         initMetricsCollect();
     }
 
+    /**
+     * Binlog for logging dictionary changes.
+     */
     private Binlog binlog;
 
+    /**
+     * Sets the binlog for logging dictionary changes.
+     *
+     * @param binlog the binlog to set
+     */
     public void setBinlog(Binlog binlog) {
         this.binlog = binlog;
     }
 
+    /**
+     * Logger for logging information and errors.
+     */
     private static final Logger log = LoggerFactory.getLogger(DictMap.class);
 
+    /**
+     * Retrieves a dictionary by its sequence number.
+     *
+     * @param seq the sequence number of the dictionary
+     * @return the dictionary with the specified sequence number, or null if not found
+     */
     public Dict getDictBySeq(int seq) {
         return cacheDictBySeq.get(seq);
     }
 
+    /**
+     * Retrieves a dictionary by its key prefix or suffix.
+     *
+     * @param keyPrefixOrSuffix the key prefix or suffix of the dictionary
+     * @return the dictionary with the specified key prefix or suffix, or null if not found
+     */
     public Dict getDict(String keyPrefixOrSuffix) {
         return cacheDict.get(keyPrefixOrSuffix);
     }
 
+    /**
+     * Puts a dictionary into the map with a key prefix or suffix.
+     * If the dictionary's sequence number already exists, a new sequence number is generated.
+     *
+     * @param keyPrefixOrSuffix the key prefix or suffix of the dictionary
+     * @param dict              the dictionary to put
+     * @return the dictionary that was put into the map
+     * @throws RuntimeException if there is an error writing the dictionary to the file or appending to the binlog
+     */
     @SlaveNeedReplay
     @SlaveReplay
     public synchronized Dict putDict(String keyPrefixOrSuffix, Dict dict) {
@@ -92,28 +149,51 @@ public class DictMap implements NeedCleanUp {
         return cacheDict.put(keyPrefixOrSuffix, dict);
     }
 
+    /**
+     * Returns a copy of the cache dictionary map.
+     *
+     * @return a copy of the cache dictionary map
+     */
     public HashMap<String, Dict> getCacheDictCopy() {
         return new HashMap<>(cacheDict);
     }
 
+    /**
+     * Returns a copy of the cache dictionary by sequence map.
+     *
+     * @return a copy of the cache dictionary by sequence map
+     */
     public TreeMap<Integer, Dict> getCacheDictBySeqCopy() {
         return new TreeMap<>(cacheDictBySeq);
     }
 
-    // worker share dict, init on start, need persist
-    // for compress
+    /**
+     * Cache for dictionaries by key prefix or suffix.
+     */
     private final ConcurrentHashMap<String, Dict> cacheDict = new ConcurrentHashMap<>();
-    // can not be removed
-    // for decompress
-    // if dict retrain, and dict count is large, it will be a problem, need clean not used dict, todo
+
+    /**
+     * Cache for dictionaries by sequence number.
+     */
     private final ConcurrentHashMap<Integer, Dict> cacheDictBySeq = new ConcurrentHashMap<>();
 
+    /**
+     * Returns the size of the dictionary map.
+     *
+     * @return the size of the dictionary map
+     */
     public int dictSize() {
         return cacheDictBySeq.size();
     }
 
+    /**
+     * File output stream for writing dictionaries to a file.
+     */
     private FileOutputStream fos;
 
+    /**
+     * Cleans up resources used by the dictionary map.
+     */
     @Override
     public synchronized void cleanUp() {
         if (fos != null) {
@@ -132,6 +212,9 @@ public class DictMap implements NeedCleanUp {
         Dict.GLOBAL_ZSTD_DICT.closeCtx();
     }
 
+    /**
+     * Clears all dictionaries from the map and truncates the dictionary file.
+     */
     public synchronized void clearAll() {
         for (var entry : cacheDictBySeq.entrySet()) {
             entry.getValue().closeCtx();
@@ -149,9 +232,22 @@ public class DictMap implements NeedCleanUp {
         }
     }
 
+    /**
+     * File name for the dictionary map file.
+     */
     private static final String FILE_NAME = "dict-map.dat";
+
+    /**
+     * Directory file for storing dictionary files.
+     */
     private File dirFile;
 
+    /**
+     * Initializes the dictionary map from a directory file.
+     *
+     * @param dirFile the directory file to initialize from
+     * @throws IOException if an I/O error occurs
+     */
     public synchronized void initDictMap(File dirFile) throws IOException {
         this.dirFile = dirFile;
         var file = new File(dirFile, FILE_NAME);
@@ -194,14 +290,29 @@ public class DictMap implements NeedCleanUp {
         Dict.initGlobalDictBytesByFile(new File(dirFile, Dict.GLOBAL_DICT_FILE_NAME));
     }
 
+    /**
+     * Updates the global dictionary bytes and saves them to a file.
+     *
+     * @param dictBytes the new global dictionary bytes
+     */
     public synchronized void updateGlobalDictBytes(byte[] dictBytes) {
         Dict.resetGlobalDictBytes(dictBytes);
         Dict.saveGlobalDictBytesToFile(new File(dirFile, Dict.GLOBAL_DICT_FILE_NAME));
     }
 
+    /**
+     * Label values for global dictionary metrics.
+     */
     private final List<String> labelValuesGlobal = List.of("global_");
+
+    /**
+     * Label values for self dictionary metrics.
+     */
     private final List<String> labelValuesSelf = List.of("self_");
 
+    /**
+     * Initializes metrics collection for dictionary compression.
+     */
     private void initMetricsCollect() {
         // only first slot show global metrics
         dictCompressedGauge.addRawGetter(() -> {
@@ -237,3 +348,4 @@ public class DictMap implements NeedCleanUp {
         });
     }
 }
+
