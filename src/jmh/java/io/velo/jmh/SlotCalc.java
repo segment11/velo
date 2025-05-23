b1829963482c7d60cbd0392b42da62a49a317e06
kerry <dingyong87@163.com>
2025-05-23 13:34:25 +0800

    perf: add crc16 benchmark test.

--- src/jmh/java/io/velo/jmh/SlotCalc.java
+++ src/jmh/java/io/velo/jmh/SlotCalc.java
@@ -3,6 +3,7 @@ package io.velo.jmh;
 import io.velo.BaseCommand;
 import io.velo.Utils;
 import org.openjdk.jmh.annotations.*;
+import redis.clients.jedis.util.JedisClusterCRC16;
 
 import java.io.IOException;
 import java.util.Random;
@@ -10,15 +11,15 @@ import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode({Mode.AverageTime})
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
-@Warmup(iterations = 1, time = 1)
-@Measurement(iterations = 1, time = 1)
+@Warmup(iterations = 1, time = 5)
+@Measurement(iterations = 1, time = 5)
 @State(Scope.Thread)
 @Threads(1)
 public class SlotCalc {
-    @Param({"1000000"})
-    int initKeyNumber = 1_000_000;
+    @Param({"10000000", "100000000"})
+    int initKeyNumber = 10_000_000;
 
-    @Param({"8", "16", "64"})
+    //    @Param({"8", "16", "64"})
     int slotNumber = 8;
 
     private final Random random = new Random();
@@ -35,10 +36,24 @@ public class SlotCalc {
         System.out.println("Done init key bytes array, key number: " + initKeyNumber);
     }
 
+    /*
+Benchmark           (initKeyNumber)  Mode  Cnt  Score   Error  Units
+SlotCalc.calc              10000000  avgt       0.236          us/op
+SlotCalc.calc             100000000  avgt       0.293          us/op
+SlotCalc.calcCrc16         10000000  avgt       0.195          us/op
+SlotCalc.calcCrc16        100000000  avgt       0.250          us/op
+     */
     @Benchmark
     public void calc() {
         int i = random.nextInt(initKeyNumber);
         var keyBytes = initKeyBytesArray[i];
         BaseCommand.slot(keyBytes, slotNumber);
+    }
+
+    @Benchmark
+    public void calcCrc16() {
+        int i = random.nextInt(initKeyNumber);
+        var keyBytes = initKeyBytesArray[i];
+        JedisClusterCRC16.getSlot(keyBytes);
     }
 }
